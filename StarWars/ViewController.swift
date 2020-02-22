b240d5e56ce6381e135c571bc0d88ee165187351
Hassan <Hassan.3skandari@gmail.com>
2020-02-22 22:11:05 +0330

    Initial Commit

---
+++ StarWars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWars
+//
+//  Created by Hassan on 2/22/20.
+//  Copyright © 2020 hassaneskandari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
